7e86a92de2692f120162a8b5fe8c5fdb788ee67f
Henry Acosta <acosta.henry@gmail.com>
2016-04-29 13:40:45 -0400

    completed project

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 /*start document ready*/
 $(document).ready(function () {
-
+// TODO: 1) when mouse is clicked, show .ryu-throwing .ryu-action 2) when mouse is clicked play sound(hadouken.mp3) 3) When x is pressed do .ryu-cool & play cool music..
 
 /*On page load---> display default state*/
 $('.ryu-action').hide();
@@ -9,7 +9,6 @@ $('.ryu-still').show();
 /* On mouse over display ryu-ready*/
 /*-->mouseenter--> display ryu-ready (and hide everything else)*/
 $('.ryu').mouseenter(function() {
-alert('mouse entered .ryu div');
 $('.ryu-action').hide();
 $('.ryu-ready').show();
 
@@ -19,15 +18,17 @@ $('.ryu').mouseleave(function() {
   $('.ryu-action').hide(); //(hides all 4 divs)*/
   $('.ryu-still').show();
 });
-
+  /*function playHadouken() {
+    alert('Max is a cool cat!');
+  }*/
 
 /*On click display ryu-throwing (hide everything else)*/
 /*--> mousedown --> display ryu-throwing (and hide everything else)*/
 $('.ryu').mousedown(function() {
    playHadouken();
-   $('ryu-action').hide();//(this line means hide all 4 divs)
-   $('.ryu-action').show();
-   $('.ryu-throwing').show();.animate({
+   $('.ryu-action').hide();//(this line means hide all 4 divs)
+   $('.ryu-throwing').show();
+   $('.hadouken').finish().show().animate({
      //this is end point for the animation
                 'left': '300px'
   },
@@ -54,7 +55,7 @@ $('.ryu').mousedown(function() {
                 if (key.keyCode == 88) {
                     $('.ryu-action').hide(); //(This line means hide all 4 divs)
                     $('.ryu-cool').show();
-                    playCool()
+                    playCool();
                 }
             });
 /*-> keyup -> display the default state --> ryu-still (and hide everything else)*/
@@ -69,3 +70,26 @@ $(document).keyup(function (key) {
 
   //close document ready
 });
+
+/*How to play a sound */
+var hadoukenSound = false;
+
+function playHadouken() {
+     // if the sound was playing before, make sure it stops before starting a new one
+    hadoukenSound = !hadoukenSound;
+    if (hadoukenSound) {
+        $('#hadouken-sound')[0].volume = 0.5; //set the volume ( 1 = 100% ; so 0.5 = 50%)
+        $('#hadouken-sound')[0].load(); //Load the sound into memory
+        $('#hadouken-sound')[0].play(); //play it
+    }
+}
+
+var coolSound = false;
+
+function playCool() {
+    //if the sound was playing before make sure it stops before starting a new one
+    coolSound = !coolSound;
+    if (coolSound) {
+        $('#cool')[0].play();
+    }
+}
